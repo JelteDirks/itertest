b9dbeee5a0577b9dde944dcb84b05b4bf378322a
Jelte Dirks <j.dirks@outlook.com>
2024-07-03 21:06:16 +0200

    fix 0 cursor issue
    Issue arises with unsigned arithmatic when the first character of a
    string is the newline character.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 
-const SOME_VALUE: &str = r#"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Dui ut ornare lectus sit amet. Diam maecenas ultricies mi eget mauris pharetra.
-Ut porttitor leo a diam sollicitudin tempor id eu nisl. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam.
-Massa tincidunt dui ut ornare lectus sit amet est placerat. Metus vulputate eu scelerisque felis imperdiet.
-Orci ac auctor augue mauris augue neque gravida. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et.
-Aliquet risus feugiat in ante metus dictum at. Scelerisque in dictum non consectetur a erat nam at.
-Quam adipiscing vitae proin sagittis nisl rhoncus. Sit amet dictum sit amet.
+const SOME_VALUE: &str = r#"HTTP/1.1 200 OK
+X-Powered-By: Express
+Content-Type: application/octet-stream
+Content-Length: 25
+ETag: W/"19-B8zw7OXzcTA2cl4FfElEEwnpEvE"
+Date: Wed, 03 Jul 2024 18:58:33 GMT
+Connection: keep-alive
+Keep-Alive: timeout=5
 
-Tempus imperdiet nulla malesuada pellentesque elit. Ornare massa eget egestas purus viverra accumsan in nisl nisi.
-Mauris in aliquam sem fringilla ut morbi. Faucibus a pellentesque sit amet porttitor eget dolor.
-Semper viverra nam libero justo laoreet sit amet cursus. Diam maecenas ultricies mi eget mauris.
-Nibh mauris cursus mattis molestie a iaculis at erat. Ullamcorper velit sed ullamcorper morbi tincidunt.
-Lorem dolor sed viverra ipsum nunc aliquet. Gravida rutrum quisque non tellus orci.
-Tincidunt dui ut ornare lectus sit amet est. Eget est lorem ipsum dolor sit amet."#;
+This is some binary data.
+"#;
 
 
 struct LineBoundaries<'a> {
@@ -53,7 +50,9 @@ impl<'a> Iterator for LineBoundaries<'a> {
             self.cursor += 1;
         }
 
-        let end = if self.buf[self.cursor - 1] == b'\r' {
+        let end = if self.cursor == 0 {
+            0
+        } else if self.buf[self.cursor - 1] == b'\r' {
             self.cursor - 1
         } else {
             self.cursor
@@ -70,11 +69,12 @@ fn main() {
 
     println!("length: {}", byte_slice.len());
 
-    let lb = LineBoundaries::new(byte_slice);
+    let mut lb = LineBoundaries::new(byte_slice);
+
+    lb.next();
 
     for boundaries in lb {
         println!("{:?}", boundaries);
-
         println!("{}", String::from_utf8(byte_slice[boundaries.0..boundaries.1].to_vec()).unwrap());
     }
 }
