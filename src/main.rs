58993a67cab049b9823db43e5c90b0aff413d321
Jelte Dirks <j.dirks@outlook.com>
2024-07-04 02:17:10 +0200

    add decoding of header strings stub: make string again
    Makes a string of the line boundary pairs again. Try it in two different
    ways, one with iter and one manually reconstructing. See which one is
    faster. Benchmark says iter is WAY faster.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ where Iter: Iterator<Item = &'a u8>
 
     fn next(&mut self) -> Option<Self::Item> {
         let start = self.cursor;
-        let mut previous: &u8 = &0;
+        let mut _previous: &u8 = &0;
 
         match self.iter.next() {
             Some(n) => {
@@ -59,7 +59,7 @@ where Iter: Iterator<Item = &'a u8>
                     self.cursor += 1;
                     return Some((self.cursor-1, self.cursor-1));
                 }
-                previous = n;
+                _previous = n;
                 self.cursor += 1;
             }
             None => return None
@@ -71,7 +71,7 @@ where Iter: Iterator<Item = &'a u8>
                     if *n == b'\n' {
                         break;
                     }
-                    previous = n;
+                    _previous = n;
                     self.cursor += 1;
                 }
                 None => {
@@ -80,7 +80,7 @@ where Iter: Iterator<Item = &'a u8>
             }
         }
 
-        let end = if *previous == b'\r' {
+        let end = if *_previous == b'\r' {
             self.cursor - 1
         } else {
             self.cursor
@@ -92,21 +92,69 @@ where Iter: Iterator<Item = &'a u8>
     }
 }
 
+struct HeaderDecoder<'a, Iter>
+where Iter: Iterator<Item = (usize, usize)>
+{
+    iter: Iter,
+    buf: &'a [u8],
+}
+
+impl<'a, Iter> HeaderDecoder<'a, Iter>
+where Iter: Iterator<Item = (usize, usize)>
+{
+    fn new(i: Iter, buf: &'a [u8]) -> Self {
+        HeaderDecoder { iter: i, buf }
+    }
+}
+
+impl<'a, Iter> Iterator for HeaderDecoder<'a, Iter>
+    where Iter: Iterator<Item = (usize, usize)>
+{
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            Some(pair) => {
+                return Some(String::from_utf8(self.buf[pair.0..pair.1].to_vec()).unwrap());
+            }
+            None => None
+        }
+    }
+}
+
+trait HeaderDecoderExt<'a>: Sized
+where Self: Iterator<Item = (usize, usize)>
+{
+    fn decode_headers(self, buf: &'a [u8]) -> HeaderDecoder<'a, Self>;
+}
+
+impl <'a, Iter> HeaderDecoderExt<'a> for Iter
+where Iter: Iterator<Item = (usize, usize)>,
+{
+    fn decode_headers(self, buf: &'a [u8]) -> HeaderDecoder<'a, Self> {
+        HeaderDecoder::new(self, buf)
+    }
+}
+
 fn main() {
-    let byte_slice: &[u8] = SOME_VALUE.as_bytes();
+    let bytes = SOME_VALUE.as_bytes();
 
-    let lb = LineBoundaries::new(byte_slice.iter());
-
-    for boundaries in lb {
-        println!("{:?}", boundaries);
-        println!("{}", String::from_utf8(byte_slice[boundaries.0..boundaries.1].to_vec()).unwrap());
+    // === TIME START ===
+    let t_start = std::time::Instant::now();
+    for boundaries in LineBoundaries::new(SOME_VALUE.as_bytes().iter()) {
+        let s = String::from_utf8(bytes[boundaries.0..boundaries.1].to_vec()).unwrap();
+        std::hint::black_box(s);
     }
+    let t1 = std::time::Instant::now() - t_start;
+    // === TIME END ===
 
-    let iterme: &[u8] = SOME_VALUE.as_bytes();
+    // === TIME START ===
+    let t_start = std::time::Instant::now();
+    SOME_VALUE.as_bytes().iter().line_boundaries().decode_headers(bytes).for_each(|s| {
+        std::hint::black_box(s);
+    });
+    let t2 = std::time::Instant::now() - t_start;
+    // === TIME END ===
 
-    for boundaries in iterme.iter().line_boundaries() {
-        println!("{:?}", boundaries);
-        println!("{}", String::from_utf8(iterme[boundaries.0..boundaries.1].to_vec()).unwrap());
-    }
-
+    println!("{},{}", t1.as_nanos(), t2.as_nanos());
 }
